dfa5f0d2c2614e3ab057e62a0c8709a2adcfed07
Arthur <47847427+77wiz77@users.noreply.github.com>
2022-10-20 22:08:44 +0300

    Update Mindmap.tsx

--- src/components/mindmap/Mindmap.tsx
+++ src/components/mindmap/Mindmap.tsx
@@ -1,10 +1,8 @@
-import React, { useState, Suspense, lazy } from 'react';
+import React, { Suspense, lazy } from 'react';
 import './Mindmap.scss';
 import Loader from '../loader/Loader';
 
 const Mindmap: React.FC = () => {
-  const [load, setLoad] = useState(true);
-
   const FrameMindMap = lazy(async () => {
     await new Promise((res) => setTimeout(res, 1000));
     return await import('./FrameMindMap');
